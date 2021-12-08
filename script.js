59b221e89e071ac9728d98cbb1dc3452832433d1
mended-marlin <Felix71679@gmail.com>
2021-12-08 15:41:38 -0500

    Working on repeating the pomodoros followed by a break. Barely functioning prototype.

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 const startTime = document.getElementById("start")
+const stopTime = document.getElementById("stop")
 const timer = document.getElementById("test")
-const workMin = document.querySelector(".wMin")
-const workSec = document.querySelector(".wSec")
-const breakMin = document.querySelector(".bMin")
-const breakSec = document.querySelector(".bSec")
 const bAdd = document.querySelector("#bTimeUp")
 const bSub = document.querySelector("#bTimeDown")
 const wAdd = document.querySelector("#wTimeUp")
 const wSub = document.querySelector("#wTimeDown")
+const workMin = document.querySelector(".wMin")
+const workSec = document.querySelector(".wSec")
+const breakMin = document.querySelector(".bMin")
+const breakSec = document.querySelector(".bSec")
 
 
-breakSec.textContent = 00;
-
-//start button functionality 
+let startTimeValue = 0;
 
 
-startTime.addEventListener("click", () => {
+stopTime.addEventListener("click", () => {
+    // if you add a boolean as a parameter to the pomodoro function it is possible to achieve a true pause of the loop. Just a small consideration and idea from you, yourself, and I. 
+    startTimeValue = 0;
+    console.log(startTimeValue)
     pomodoro();
 })
 
+//Setting wait time between pomodoros
+let setBreakTime = [breakMin.textContent, breakSec.textContent]
+let setWorkTime =  [workMin.textContent, workSec.textContent]
+        var a = setBreakTime[0]
+        var b = setBreakTime[1]
+        var c = setWorkTime[0]
+        var d = setWorkTime[1]
 
-//Time removal and addition buttons 
-bAdd.addEventListener("click", () => {
-    let bMin = parseInt(breakMin.textContent)
-    let bSec = parseInt(breakSec.textContent)
-    if (bSec>=45){
-        breakSec.textContent = "00"
-        breakMin.textContent = bMin + 20;
-    }else{
-        breakSec.textContent = bSec + 15
+        let waitTime = ((a*60000)+(b*1000)) + ((c*60000)+(d*1000))
+
+
+
+//start button functionality 
+startTime.addEventListener("click", () => {
+    
+    startTimeValue = 1
+    console.log(startTimeValue)
+
+    function delay(time){
+        return new Promise(resolve => setTimeout(resolve,time));
+}
+    async function runCycles(){
+        pomodoro()
+        await delay(waitTime);
+        pomodoro();
     }
 
+    runCycles();
+    
 })
+
+
+
+
 
 
 
